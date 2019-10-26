da16b8a04004f737a7a54371858b6e7c9799142d
Down To Earth Astronomy <DownToEarthAstronomy@gmail.com>
2019-10-26 17:58:01 +0200

    Removing debug code

--- lib/Get-LoadoutList.ps1
+++ lib/Get-LoadoutList.ps1
@@ -25,12 +25,5 @@ Function Get-LoadoutList{
     Return $LoadoutList
 }
 
-. '\\SPACEVAULT\Down_To_Earth_Astronomy\scripts\Shield_tester\lib\One-Up.ps1'
-
-#Write-host $(Get-Date)
-#$a = Get-LoadoutList -ShieldBoosterCount 6 -ShieldBoosterVariants 20
-#$a = Get-LoadoutList -ShieldBoosterCount 8 -ShieldBoosterVariants 12
-#Write-host $(Get-Date)
-
 
 
