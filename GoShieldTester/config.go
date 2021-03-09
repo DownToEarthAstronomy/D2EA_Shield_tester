6cfac256ba124afe828b4b951f9526a3ac4f375a
brabrah <brabrah@protonmail.com>
2021-03-09 07:55:07 +0100

    Edited config and rebuilt

--- GoShieldTester/config.go
+++ GoShieldTester/config.go
@@ -26,8 +26,8 @@ func loadConfig() configT {
 		thermalDPS:             50,   // laser weapons
 		absoluteDPS:            0,    // 0 for most NPCs except Thargoids, 100 for a Cyclops, 150 for a Basilisk, 200 for a Hydra
 		damageEffectiveness:    65,   // 25 = fixed weapons or single enemy, 65 = hazrez, 100% always being attacked
-		scbHitPoint:            7335, // Using Corolis, enter the total in the Defense > Shield Sources > Cells green pie slice. e.g. 2 x 7A SCBs is 3621 MJ *2 = 7242 MJ
-		guardianShieldHitPoint: 827,  // Using Corolis, enter the total in the Defense > Shield Sources > Shield Additions yellow pie slice. e.g. 1 x 5D GSRB = 215 MJ
+		scbHitPoint:            3667, // Using Corolis, enter the total in the Defense > Shield Sources > Cells green pie slice. e.g. 2 x 7A SCBs is 3621 MJ *2 = 7242 MJ
+		guardianShieldHitPoint: 942,  // Using Corolis, enter the total in the Defense > Shield Sources > Shield Additions yellow pie slice. e.g. 1 x 5D GSRB = 215 MJ
 		boosterFile:            "../lib/ShieldBoosterVariants.csv",
 		generatorFile:          "../lib/ShieldGeneratorVariants.csv",
 		shipFile:               "../lib/ShipStats.csv",
